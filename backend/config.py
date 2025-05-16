627879cb7c6bb9825988a656dbb7eec856fd0858
kadeem-online <kadeemalexander254@gmail.com>
2025-05-16 11:09:15 +0300

    UPDATE: added configurations for the database URI and secret

--- backend/config.py
+++ backend/config.py
@@ -8,25 +8,45 @@ class DefaultConfig():
     configurations should inherit from this as it contains the required
     configurations fields.
     """
+    # basic configuration
     DEBUG: bool = False
     TESTING: bool = False
+    SECRET_KEY: str = "notr_secret_key"
+
+    # database configuration
+    SQLALCHEMY_DATABASE_URI: str = "sqlite:///notr_database.db"
 
 
 class DevelopmentConfig(DefaultConfig):
     """
     The development configuration settings.
     """
+    # basic configuration
     DEBUG = True
+    SECRET_KEY = os.getenv("SECRET_KEY", "dev_secret_key")
+
+    # database configuration
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URI", "sqlite:///notr_dev_databse.db"
+    )
 
 
 class ProductionConfig(DefaultConfig):
     """
     The production configuration settings.
     """
+    # basic configuration
+    SECRET_KEY = os.getenv("SECRET_KEY", "prod_secret_key")
+
+    # database configuration
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URI", "sqlite:///notr_dev_databse.db"
+    )
 
 
 class TestingConfig(DefaultConfig):
     """
     The testing configuration settings.
     """
+    # basic configuration
     TESTING = True
