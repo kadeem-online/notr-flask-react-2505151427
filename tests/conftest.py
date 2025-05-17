b8efe8986e1969366db17b2d97ead775735cdd81
kadeem-online <kadeemalexander254@gmail.com>
2025-05-17 15:54:12 +0300

    TEST: added a temporary file to be used for test app database.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,9 @@
+# standard library imports
+import tempfile
+
 # third party imports
 import pytest
+from flask_migrate import upgrade
 
 # local application imports
 from backend import create_app
@@ -8,7 +12,18 @@ from backend.extensions import database
 
 @pytest.fixture
 def app():
-    app = create_app("testing")
+    """
+    Creates the tests application instance.
+    """
+
+    # create temporary database file
+    db_fd, db_path = tempfile.mkstemp(".db")
+
+    app = create_app(test_config={
+        "TESTING": True,
+        "SQLALCHEMY_DATABASE_URI": f"sqlite:///{db_path}",
+        "SQLALCHEMY_TRACK_MODIFICATIONS": False
+    })
     with app.app_context():
         database.create_all()
         yield app
