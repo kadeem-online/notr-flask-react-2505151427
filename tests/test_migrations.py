678f6ffcd545418b7ac427680b94352fc6d752b2
kadeem-online <kadeemalexander254@gmail.com>
2025-05-17 15:56:10 +0300

    TEST: fixed migration test, moved from using app fixture to using a clean app instance.

--- tests/test_migrations.py
+++ tests/test_migrations.py
@@ -1,10 +1,22 @@
+# standard library imports
+import tempfile
+
 # third party imports
 from flask_migrate import upgrade
 
+# local application imports
+from backend import create_app
 
-def test_migrations(app):
+
+def test_migrations():
     """
     Check if applying migrations works.
     """
+    _, db_path = tempfile.mkstemp(".db")
+
+    app = create_app(test_config={
+        "SQLALCHEMY_DATABASE_URI": f"sqlite:///{db_path}"
+    })
+
     with app.app_context():
         upgrade()
