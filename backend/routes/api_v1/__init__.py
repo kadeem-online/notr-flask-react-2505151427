824854a3a534942b78e7347a8993a621f1ac69dc
kadeem-online <kadeemalexander254@gmail.com>
2025-05-27 10:21:39 +0300

    UPDATE: registered the auth subroute with the api v1 route

--- backend/routes/api_v1/__init__.py
+++ backend/routes/api_v1/__init__.py
@@ -2,13 +2,17 @@
 from flask import Blueprint
 
 # local application imports
+import backend.routes.api_v1.auth as auth_routes
 from backend.utilities.functions import formatResponse
 
 # Blueprint for API v1 routes
 blueprint = Blueprint('api_v1', __name__, url_prefix='/api/v1')
 
+# Register subroutes
+blueprint.register_blueprint(auth_routes.blueprint)
 
-@blueprint.route('/', methods=['GET'], strict_slashes=False)
+
+@blueprint.route('/', methods=['GET', 'POST'], strict_slashes=False)
 def index():
     """
     Index route for the API v1.
